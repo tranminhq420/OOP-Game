ca6a37527c4194c4f23e7e1e2c02ea4691fa74c0
huylinh <nguyenhuylinhvt98@gmail.com>
2022-07-10 22:37:02 +0700

    Thay doi hero direction va bo dx dy

--- src/Entities/Monster.java
+++ src/Entities/Monster.java
@@ -2,19 +2,18 @@ package Entities;
 
 import java.util.Random;
 
+import Entities.GameObjectDynamic.Direction;
 import Main.Board;
 
 public class Monster {
-	Random rd = new Random();
-	private int timez = 0;
+
 	private final int speed = 1;
 	private int maxLife = 4;
 	private int life = maxLife;
 	private int attack = 2;
 	private int defense = 0;
-	private boolean collision = true;
 	private GameObjectDynamic monsterGP;
-
+	private int timez = 0;
 	public Monster(int x, int y) {
 		setMonsterGP(new GameObjectDynamic(x, y));
 		initMonster();
@@ -23,26 +22,38 @@ public class Monster {
 	private void initMonster() {
 		monsterGP.loadImage("res/textures/img/tauvutru.png");
 		monsterGP.getImageDimension();
+		monsterGP.setObjectDricetion(Direction.DOWN);
 	}
 
 	public void move() {
-		if (monsterGP.getDirect() == 1) {
+		if (monsterGP.getObjectDricetion() == Direction.RIGHT) {
 			monsterGP.x += speed;
 			monsterGP.loadImage("res/textures/img/monkeyright.png");
-		} else if (monsterGP.getDirect() == -1) {
+		} else if (monsterGP.getObjectDricetion() == Direction.LEFT) {
 			monsterGP.x -= speed;
 			monsterGP.loadImage("res/textures/img/monkeyleft.png");
-		} else if (monsterGP.getDirect() == 2) {
+		} else if (monsterGP.getObjectDricetion() == Direction.UP) {
 			monsterGP.y -= speed;
 			monsterGP.loadImage("res/textures/img/monkeyup.png");
-		} else if (monsterGP.getDirect() == -2) {
+		} else if (monsterGP.getObjectDricetion() == Direction.DOWN) {
 			monsterGP.y += speed;
 			monsterGP.loadImage("res/textures/img/monkeydown.png");
 		}
+		
+		Random rd = new Random();
+
 		timez += 1;
 		if (timez == 100) { // cứ sau 100 chu kỳ timer.DELAY lại chuyển hướng di chuyển
-			monsterGP.setDirect(rd.nextInt(5) - 2); // random hướng di chuyển (0..5 -2 --> -2 ..2 hướng di chuyển đã quy
-																										// định 0
+		 // random hướng di chuyển 
+			int key = rd.nextInt(4);// định 0
+			switch (key) {
+			case 4:	monsterGP.setObjectDricetion(Direction.DOWN);break;
+			case 1:	monsterGP.setObjectDricetion(Direction.LEFT);break;
+			case 2:	monsterGP.setObjectDricetion(Direction.UP);break;
+			case 3:	monsterGP.setObjectDricetion(Direction.RIGHT);break;
+
+			}
+			System.out.println(key);
 			// tương ứng với đứng yên)
 			timez = 0;
 		}
@@ -59,84 +70,6 @@ public class Monster {
 		if (monsterGP.y > Board.getSizeY() - monsterGP.height) {
 			monsterGP.y = Board.getSizeY() - monsterGP.height;
 		}
-
-//		int left = Board.m.checkMapLeft(monsterGP.x, monsterGP.y, monsterGP.width, monsterGP.height);
-//		int right = Board.m.checkMapRight(monsterGP.x, monsterGP.y, monsterGP.width, monsterGP.height);
-//		int up = Board.m.checkMapUp(monsterGP.x, monsterGP.y, monsterGP.width, monsterGP.height);
-//		int down = Board.m.checkMapDown(monsterGP.x, monsterGP.y, monsterGP.width, monsterGP.height);
-//
-//		GameObject objectLeft = new GameObject(0, 0, 0, down, null);
-//		switch (left) {
-//			case 0:
-//				objectLeft.collision = new Earth(0, 0, 0, down, null).getCollision();
-//				break;
-//			case 1:
-//				objectLeft.collision = new Tree(0, 0, 0, down, null).getCollision();
-//				break;
-//			case 2:
-//				objectLeft.collision = new Water(0, 0, 0, down, null).getCollision();
-//				break;
-//			case 3:
-//				objectLeft.collision = new Rock(0, 0, 0, down, null).getCollision();
-//				break;
-//			case 9:
-//				objectLeft.collision = new NewLandDoor(0, 0, 0, down, null).getCollision();
-//				break;
-//		}
-//		GameObject objectRight = new GameObject(0, 0, 0, down, null);
-//		switch (right) {
-//			case 0:
-//				objectRight.collision = new Earth(0, 0, 0, down, null).getCollision();
-//				break;
-//			case 1:
-//				objectRight.collision = new Tree(0, 0, 0, down, null).getCollision();
-//				break;
-//			case 2:
-//				objectRight.collision = new Water(0, 0, 0, down, null).getCollision();
-//				break;
-//			case 3:
-//				objectRight.collision = new Rock(0, 0, 0, down, null).getCollision();
-//				break;
-//			case 9:
-//				objectRight.collision = new NewLandDoor(0, 0, 0, down, null).getCollision();
-//				break;
-//		}
-//		GameObject objectUp = new GameObject(0, 0, 0, down, null);
-//		switch (up) {
-//			case 0:
-//				objectUp.collision = new Earth(0, 0, 0, down, null).getCollision();
-//				break;
-//			case 1:
-//				objectUp.collision = new Tree(0, 0, 0, down, null).getCollision();
-//				break;
-//			case 2:
-//				objectUp.collision = new Water(0, 0, 0, down, null).getCollision();
-//				break;
-//			case 3:
-//				objectUp.collision = new Rock(0, 0, 0, down, null).getCollision();
-//				break;
-//			case 9:
-//				objectUp.collision = new NewLandDoor(0, 0, 0, down, null).getCollision();
-//				break;
-//		}
-//		GameObject objectDown = new GameObject(0, 0, 0, down, null);
-//		switch (down) {
-//			case 0:
-//				objectDown.collision = new Earth(0, 0, 0, down, null).getCollision();
-//				break;
-//			case 1:
-//				objectDown.collision = new Tree(0, 0, 0, down, null).getCollision();
-//				break;
-//			case 2:
-//				objectDown.collision = new Water(0, 0, 0, down, null).getCollision();
-//				break;
-//			case 3:
-//				objectDown.collision = new Rock(0, 0, 0, down, null).getCollision();
-//				break;
-//			case 9:
-//				objectDown.collision = new NewLandDoor(0, 0, 0, down, null).getCollision();
-//				break;
-//		}
 
 		if (checkMapLeft(monsterGP.x, monsterGP.y, monsterGP.width, monsterGP.height)) {
 			monsterGP.x = monsterGP.x + speed;
--- src/Entities/Stone.java
+++ src/Entities/Stone.java
@@ -9,10 +9,10 @@ public class Stone extends Bullet{
 	@Override
 	
     public void move() {
-    	if (getDirect() == 1) x += bullet_speed; // viên đạn bay ngang
-        if (getDirect() == 2) y -= bullet_speed; // viên đạn bay d�?c 
-        if (getDirect() ==-1) x -= bullet_speed;
-        if (getDirect() ==-2) y += bullet_speed;
+    	if (getObjectDricetion() == Direction.RIGHT ) x += bullet_speed; // viên đạn bay ngang
+        if (getObjectDricetion() == Direction.UP) y -= bullet_speed; // viên đạn bay d�?c 
+        if (getObjectDricetion() == Direction.LEFT ) x -= bullet_speed;
+        if (getObjectDricetion() == Direction.DOWN) y += bullet_speed;
         fly += bullet_speed; // so sánh độ dài đã bay với độ dài đạn
         if (fly>bullet_length) {
         	setTontai(false);
